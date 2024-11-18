dbec1f38b5d9105c93e29c34cd29eaf69a9caa6e
halfmoon01 <onurivit01@gmail.com>
2024-11-19 02:20:18 +0900

    commit3: Build Home Page

--- assignment4/src/App.jsx
+++ assignment4/src/App.jsx
@@ -5,6 +5,7 @@ import React, {useState, useEffect} from 'react';
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
@@ -12,6 +13,10 @@ function App() {
     <Router>
       <Navbar />
       <div className="container">
+      <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
       </div>
     </Router>
   );
---
+++ assignment4/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+//Sanghyun Jun
+//Sanghyun.Jun.1@stonybrook.edu
+
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <ul className="main-list">
+        <li>Facility List</li>
+        <ul className="sub-list">
+          <li>Show list of facilities.</li>
+          <li>name, description, location, min capacity, max capacity, image, onlySUNY, available day of the week</li>
+        </ul>
+        <li>Facility Reservation</li>
+        <ol className="numbered-list">
+          <li>Reservation Date should be the date after today</li>
+          <li>The number of users should be between the maximum number of people and the minimum number of people</li>
+          <li>If the facility is available only for SUNY Korea, user should be in SUNY Korea</li>
+          <li>The reservation date must be made on the available day of the week</li>
+          <li>If someone booked the facility on that date, no one else can book the facility on that date</li>
+        </ol>
+        <ul className="sub-list">
+          <li>If all conditions are met, data is stored in the database</li>
+        </ul>
+        <li>User</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
+
